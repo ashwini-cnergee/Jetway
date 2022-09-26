955d3e94af56d862e3d61bb4287ca883aff5fc31
ashwini-cnergee <ashwini@cnergee.com>
2022-09-26 14:47:49 +0530

    Jetway

---
+++ app/src/main/java/com/cnergee/mypage/obj/AuthenticationMobile.java
@@ -0,0 +1,9 @@
+package com.cnergee.mypage.obj;
+
+public class AuthenticationMobile {
+
+	public static String CliectAccessId="CM000019JW";//CM000001DB
+	public static String CliectAccessName="CliectAccessId";
+	public static String PROJECT_NUMBER="221496674432";
+
+}
